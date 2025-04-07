431fae19b7ced29320b824492b14815dbbfb048f
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-07 11:42:36 +0900

    [Feat] Add BaseScene
    게임의 기반이 될 씬 클래스 생성

---
+++ MyTextRPG/MyTextRPG/BaseScene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG
+{
+    public abstract class BaseScene
+    {
+        protected ConsoleKey input;
+        public abstract void Render();
+
+        public void Input()
+        {
+            input = Console.ReadKey().Key;
+        }
+
+        public abstract void Result();
+    }
+}
