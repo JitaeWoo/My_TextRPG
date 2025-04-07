c88b6b291ce94f2d0919e5bfec9d4fe830709893
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-07 11:55:48 +0900

    [Feat] Add Class Game
    게임에서 실행을 담당할 Game 클래스 생성

---
+++ MyTextRPG/MyTextRPG/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG
+{
+    public static class Game
+    {
+        private static Dictionary<string, BaseScene> _sceneDic = new Dictionary<string, BaseScene>();
+        private static BaseScene _curScene;
+        private static bool _gameOver;
+        public static void Run()
+        {
+            _gameOver = false;
+
+            while (!_gameOver)
+            {
+                Console.Clear();
+                _curScene.Render();
+                Console.WriteLine();
+                _curScene.Input();
+                Console.WriteLine();
+                _curScene.Result();
+                Console.WriteLine();
+            }
+
+            PrintGameOver();
+        }
+
+        public static void ChangeScene(BaseScene scene)
+        {
+            _curScene = scene;
+        }
+
+        public static void GameOver()
+        {
+            _gameOver = false;
+        }
+
+        private static void PrintGameOver()
+        {
+            Console.WriteLine("Game Over!");
+        }
+    }
+}
