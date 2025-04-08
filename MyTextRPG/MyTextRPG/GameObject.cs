0628b2a90e73d21bd47c96be8cf30f4e96a87536
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-08 12:33:28 +0900

    [Feat] IInteractable, GameObject 추가
    필드맵에서 플레이어와 상호작용 하게 될 오브젝트들의 기반인 GameObject 클래스 추가.
    주어진 솏성에 따라 맵에 출력되고 Interact를 구현함으로써 기능을 발휘한다.

---
+++ MyTextRPG/MyTextRPG/GameObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG
+{
+    public abstract class GameObject : IInteractable
+    {
+        private ConsoleColor _color;
+        private char _symbol;
+        private Vector2 _position;
+        private bool _isOnce;
+
+        public GameObject(ConsoleColor color, char symbol, Vector2 position, bool isOnce = false)
+        {
+            _color = color;
+            _symbol = symbol;
+            _position = position;
+            _isOnce = isOnce;
+        }
+
+        public void Print()
+        {
+            Console.SetCursorPosition(_position.x, _position.y);
+            Console.ForegroundColor = _color;
+            Console.Write(_symbol);
+            Console.ResetColor();
+        }
+
+        public abstract void Interact(Player player);
+    }
+}
