e3e0f84bf9b8a5061dc49c2d677bb97ec4edd3ab
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-08 14:32:51 +0900

    [Feat] Potion 추가
    플레이어의 체력을 회복하는 기능의 Item인 Potion 추가

---
+++ MyTextRPG/MyTextRPG/GameObjects/Items/Potion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG.GameObjects.Items
+{
+    public class Potion : Item
+    {
+        public Potion(Vector2 position) : base(position)
+        {
+            Name = "포션";
+            Description = "플레이어의 체력을 소량 회복한다.";
+        }
+
+        public override void Use()
+        {
+            // TODO : 플레이어 체력 회복
+        }
+    }
+}
