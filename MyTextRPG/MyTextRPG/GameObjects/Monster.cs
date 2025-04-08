1b7c12dad0ca47703af8ca11c13574ea66682d3a
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-08 17:37:17 +0900

    [Feat] Monster 추가
    앞으로 싸울 몬스터들의 기반이 될 Monster 클래스 추가
    
    플레이어를 공격하기와 공격 받기 기능이 있다.

---
+++ MyTextRPG/MyTextRPG/GameObjects/Monster.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG.GameObjects
+{
+    public abstract class Monster : GameObject
+    {
+        public string Name { get; protected set; }
+
+        protected int Attack;
+        protected int Defense;
+        protected int CurHp;
+        protected int MaxHp;
+
+        public Monster(Vector2 position, int attack, int defense, int hp) 
+            : base(ConsoleColor.Red, 'M', position, true)
+        {
+            Attack = attack;
+            Defense = defense;
+            MaxHp = hp;
+            CurHp = MaxHp;
+        }
+
+        public override void Interact()
+        {
+            // 몬스터와 전투 돌입
+        }
+
+        public virtual void AttackPlayer()
+        {
+            Game.Player.TakeDamage(Attack);
+        }
+
+        public void TakeDamage()
+        {
+            int damage = Game.Player.Attack - Defense;
+        }
+    }
+}
