7ae4f2b804cac8fbd7ea5893fc74ecabad327403
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-09 14:16:03 +0900

    [Feat] Weapon 클래스 추가
    플레이어가 장착할 무기를 담당할 Weapon 클래스 추가.

---
+++ MyTextRPG/MyTextRPG/GameObjects/Items/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG.GameObjects.Items
+{
+    public class Weapon : Item
+    {
+        private int _attack;
+        public int Attack => _attack;
+
+        public Weapon(string name, string description, int attack) 
+            : base(new Vector2(1, 1), Types.Weapon)
+        {
+            Name = name;
+            Description = description;
+            _attack = attack;
+        }
+        public Weapon(Vector2 position, string name, string description, int attack) 
+            : base(position, Types.Weapon)
+        {
+            Name = name;
+            Description = description;
+            _attack = attack;
+        }
+
+        public override void Use()
+        {
+            // 플레이어 무기 슬롯에 장착
+        }
+    }
+}
