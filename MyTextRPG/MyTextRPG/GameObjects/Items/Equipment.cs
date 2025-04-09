125d058fd35c7509d2dfbc06fdd9ee2dba580329
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-09 16:40:26 +0900

    [Refactor] Equipment 쓰지 않는 생성자 삭제
    Equipment에서 쓰지 않는 생성자를 삭제 조치

--- MyTextRPG/MyTextRPG/GameObjects/Items/Equipment.cs
+++ MyTextRPG/MyTextRPG/GameObjects/Items/Equipment.cs
@@ -8,11 +8,6 @@ namespace MyTextRPG.GameObjects.Items
 {
     public class Equipment : Item
     {
-        public Equipment(Types type)
-            : this(new Vector2(1, 1), type)
-        {
-        }
-
         public Equipment(Vector2 position, Types type) 
             : base(position, type)
         {
