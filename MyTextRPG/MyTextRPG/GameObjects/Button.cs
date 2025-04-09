e994f63fe827a3bfd26cc30d51f78b1a1c4ad5b1
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-09 18:06:14 +0900

    [Feat] Button 클래스 추가
    필드맵에서 버튼이 눌렸을 때 지정된 효과를 내는 Button GameObject 클래스 추가.

---
+++ MyTextRPG/MyTextRPG/GameObjects/Button.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG.GameObjects
+{
+    public class Button : GameObject
+    {
+        public event Action OnClicked;
+        public Button(Vector2 position) 
+            : base(ConsoleColor.Magenta, 'B', position, false)
+        {
+        }
+
+        public override void Interact()
+        {
+            OnClicked?.Invoke();
+        }
+    }
+}
