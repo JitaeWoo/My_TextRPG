0d2b1cd7496052b350b52fa6ffc749f5264b3ac5
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-09 15:38:39 +0900

    [Move] Equipments 폴더 생성 및 파일 이동
    Equipment를 상속받는 객체를 위한 Equipments 폴더 생성

--- MyTextRPG/MyTextRPG/GameObjects/Items/Equipments/Weapon.cs
+++ MyTextRPG/MyTextRPG/GameObjects/Items/Equipments/Weapon.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyTextRPG.GameObjects.Items
+namespace MyTextRPG.GameObjects.Items.Equipments
 {
     public class Weapon : Equipment
     {
