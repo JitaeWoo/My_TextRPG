57794480cdab76f0897f87c5026e85f577627c59
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-09 14:20:39 +0900

    [Feat] Armor 클래스 추가
    플레이어가 장착할 방어구를 담당할 Armor 클래스 추가

---
+++ MyTextRPG/MyTextRPG/GameObjects/Items/Armor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG.GameObjects.Items
+{
+    public class Armor : Item
+    {
+        private int _defense;
+        public int Defense => _defense;
+
+        public Armor(string name, string description, int defense) 
+            : base(new Vector2(1, 1), Types.Armor)
+        {
+            Name = name;
+            Description = description;
+            _defense = defense;
+        }
+
+        public Armor(Vector2 position, string name, string description, int defense) 
+            : base(position, Types.Armor)
+        {
+            Name = name;
+            Description = description;
+            _defense = defense;
+        }
+
+        public override void Use()
+        {
+            // 플레이어 방어구 슬롯에 장착
+        }
+    }
+}
