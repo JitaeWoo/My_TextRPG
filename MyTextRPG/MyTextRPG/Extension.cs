0df99e6813b5e3ca056623dc9b18f1caafcad411
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-09 10:44:22 +0900

    [Feat] Util 클래스 PressAnyKey 추가
    아무키나 눌러서 계속을 실행하는 PressAnyKey 추가

---
+++ MyTextRPG/MyTextRPG/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG
+{
+    class Extension
+    {
+    }
+
+    public static class Util
+    {
+        public static void PressAnyKey() => PressAnyKey("아무키나 눌러서 계속...");
+
+        public static void PressAnyKey(string text)
+        {
+            Console.WriteLine(text);
+            Console.ReadKey(true);
+        }
+    }
+}
