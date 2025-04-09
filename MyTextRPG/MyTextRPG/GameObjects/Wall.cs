cf41a57e91cfc8965fee024bb0ebe05aeb24f931
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-09 17:52:25 +0900

    [Feat] Wall 클래스 추가
    필드맵에서 플레이어를 일정 방향으로 밀어낼 Wall GameObject 추가

---
+++ MyTextRPG/MyTextRPG/GameObjects/Wall.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG.GameObjects
+{
+    class Wall : GameObject
+    {
+        private Directions _directoin;
+        public Wall(Vector2 position, Directions direction) 
+            : base(ConsoleColor.Gray, 'W', position, false)
+        {
+            _directoin = direction;
+        }
+
+        public override void Interact()
+        {
+            switch (_directoin)
+            {
+                case Directions.Up:
+                    Game.Player.Position = new Vector2(Game.Player.Position.x, Game.Player.Position.y - 1);
+                    break;
+                case Directions.Down:
+                    Game.Player.Position = new Vector2(Game.Player.Position.x, Game.Player.Position.y + 1);
+                    break;
+                case Directions.Left:
+                    Game.Player.Position = new Vector2(Game.Player.Position.x - 1, Game.Player.Position.y);
+                    break;
+                case Directions.Right:
+                    Game.Player.Position = new Vector2(Game.Player.Position.x + 1, Game.Player.Position.y);
+                    break;
+            }
+        }
+    }
+}
