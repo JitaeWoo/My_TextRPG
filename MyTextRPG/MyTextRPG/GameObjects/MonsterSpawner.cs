256656b36a575a82005ae1b38579be2603e958f4
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-10 12:35:53 +0900

    [Feat] MonsterSpawner 클래스 추가
    여관에서 잠을 잘 때마다 몬스터를 스폰하는 스포너 클래스 추가.
    
    Game의 OnSleeped 이벤트를 활용하여 몬스터를 스폰하는 기능 구현

---
+++ MyTextRPG/MyTextRPG/GameObjects/MonsterSpawner.cs
@@ -0,0 +1,44 @@
+﻿using MyTextRPG.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG.GameObjects
+{
+    class MonsterSpawner : GameObject
+    {
+        private MonsterGroup _monsterGroup;
+        private bool isSpawned = true;
+
+        public MonsterSpawner(Vector2 position, Monster monster) 
+            : this(position, new MonsterGroup(monster))
+        {
+        }
+
+        public MonsterSpawner(Vector2 position, MonsterGroup monsterGroup)
+            : base(ConsoleColor.Red, 'M', position, false)
+        {
+            _monsterGroup = monsterGroup;
+
+            Game.OnSleeped += Spawn;
+        }
+
+        public override void Interact()
+        {
+            if (isSpawned)
+            {
+                Game.ChangeScene(new BattleScene(_monsterGroup.MonsterList));
+                Symbol = ' ';
+                isSpawned = false;
+            }
+        }
+
+        public void Spawn()
+        {
+            isSpawned = true;
+            Symbol = 'M';
+        }
+    }
+}
