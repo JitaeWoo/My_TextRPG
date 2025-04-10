07dca0f0d9ba6aca4215e5265994eae12ef3cdcb
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-10 12:25:20 +0900

    [Feat] MonsterGroup 생성자 추가
    MonsterGroup 생성시 매개변수로 Monster가 올 수 있도록 추가.
    
    MonsterGroup과 Monster간의 연계성을 높이기 위함.

--- MyTextRPG/MyTextRPG/GameObjects/MonsterGroup.cs
+++ MyTextRPG/MyTextRPG/GameObjects/MonsterGroup.cs
@@ -1,4 +1,5 @@
-﻿using MyTextRPG.Scenes;
+﻿using MyTextRPG.GameObjects.Items;
+using MyTextRPG.Scenes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,18 @@ namespace MyTextRPG.GameObjects
     class MonsterGroup : GameObject
     {
         public List<Monster> MonsterList = new List<Monster>();
-        public MonsterGroup(Vector2 position) 
+        public MonsterGroup(Monster monster = null)
+            : this(new Vector2(1, 1), monster)
+        {
+        }
+
+        public MonsterGroup(Vector2 position, Monster monster = null)
             : base(ConsoleColor.Red, 'M', position, true)
         {
+            if(monster != null)
+            {
+                MonsterList.Add(monster);
+            }
         }
 
         public override void Interact()
