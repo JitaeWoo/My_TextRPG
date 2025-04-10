a98a066049ef3d9a86d7d74541f562e57514a1f8
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-10 11:26:17 +0900

    [Feat] MonsterGroup 클래스 추가
    다수의 몬스터를 표현한 GameObject인 MonsterGroup 추가.
    
    조우 시 일대다 전투를 시작할 용도로 구현

---
+++ MyTextRPG/MyTextRPG/GameObjects/MonsterGroup.cs
@@ -0,0 +1,23 @@
+﻿using MyTextRPG.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRPG.GameObjects
+{
+    class MonsterGroup : GameObject
+    {
+        public List<Monster> MonsterList = new List<Monster>();
+        public MonsterGroup(Vector2 position) 
+            : base(ConsoleColor.Red, 'M', position, true)
+        {
+        }
+
+        public override void Interact()
+        {
+            Game.ChangeScene(new BattleScene(MonsterList));
+        }
+    }
+}
